c0ba7fa555b87c895150b8eb7afbf4065eb8b761
arafataft <arafatcd@gmail.com>
2023-07-24 17:30:45 +0600

    apis

--- src/Pages/MyCollege/MyCollege.jsx
+++ src/Pages/MyCollege/MyCollege.jsx
@@ -1,79 +1,104 @@
-import  { useState } from 'react';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { AuthContext } from '../../Providers/AuthProvider';
+import { useForm, Controller } from 'react-hook-form';
 
-const collegeDetails = [
-    {
-      name: 'Sample College 1',
-      address: '123 College Street, Cityville, State 12345',
-      phone: '(123) 456-7890',
-      // Add more fields as needed for other college details
-    },
-    
-    // Add more college objects here...
-  ];
 const MyCollege = () => {
-  const [reviewRating, setReviewRating] = useState(0);
-  const [reviewText, setReviewText] = useState('');
+  const [collegeDetails, setCollegeDetails] = useState(null);
+  const { user } = useContext(AuthContext);
 
-  const handleSubmitReview = (e) => {
-    e.preventDefault();
-    // Here, you can perform any logic to submit the review.
-    // For this example, we'll just reset the review fields.
-    setReviewRating(0);
-    setReviewText('');
+  const { handleSubmit, control, reset } = useForm();
+  const onSubmit = (data) => {
+    // Add the college name to the form data before submitting
+    data.collegeName = collegeDetails.collegeName;
+    data.name = collegeDetails.name;
+    data.email = collegeDetails.email;
+
+    axios
+      .post('http://localhost:3000/reviews', data)
+      .then((response) => {
+        console.log('Review submitted successfully!', response.data);
+        // Reset the form after successful submission
+        reset();
+      })
+      .catch((error) => {
+        console.error('Error submitting review:', error);
+      });
   };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/mycollege?email=${user?.email}`)
+      .then((response) => {
+        setCollegeDetails(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching college details:', error);
+      });
+  }, [user?.email]);
 
   return (
     <Container>
-      <Row>
-        <Col>
-          <h2>My College Details</h2>
-          <p>
-            <strong>College Name:</strong> {collegeDetails[0].name}
-          </p>
-          <p>
-            <strong>Address:</strong> {collegeDetails[0].address}
-          </p>
-          <p>
-            <strong>Phone:</strong> {collegeDetails[0].phone}
-          </p>
-          {/* Display other college details as needed */}
-        </Col>
-      </Row>
+      {collegeDetails ? (
+        <Row>
+          <Col>
+            <h2>My College Details</h2>
+            <p>
+              <strong>College Name:</strong> {collegeDetails.collegeName}
+            </p>
+            <p>
+              <strong>Address:</strong> {collegeDetails.address}
+            </p>
+            <p>
+              <strong>Phone:</strong> {collegeDetails.phone}
+            </p>
+          </Col>
+        </Row>
+      ) : (
+        <p>
+          You have not been admitted yet. It is essential to complete the admission process before proceeding any further.
+        </p>
+      )}
       <Row>
         <Col>
           <h2>Add a Review</h2>
-          <Form onSubmit={handleSubmitReview}>
-            <Form.Group controlId="reviewRating">
-              <Form.Label>Rating</Form.Label>
-              <Form.Control
-                as="select"
-                value={reviewRating}
-                onChange={(e) => setReviewRating(e.target.value)}
-                required
-              >
-                <option value="0">Select Rating</option>
-                <option value="1">1 - Poor</option>
-                <option value="2">2 - Fair</option>
-                <option value="3">3 - Good</option>
-                <option value="4">4 - Very Good</option>
-                <option value="5">5 - Excellent</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="reviewText">
-              <Form.Label>Review</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={reviewText}
-                onChange={(e) => setReviewText(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit Review
-            </Button>
-          </Form>
+          {collegeDetails && (
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group controlId="reviewRating">
+                <Form.Label>Rating</Form.Label>
+                <Controller
+                  name="reviewRating"
+                  control={control}
+                  defaultValue={0}
+                  render={({ field }) => (
+                    <Form.Control as="select" {...field} required>
+                      <option value="0">Select Rating</option>
+                      <option value="1">1 - Poor</option>
+                      <option value="2">2 - Fair</option>
+                      <option value="3">3 - Good</option>
+                      <option value="4">4 - Very Good</option>
+                      <option value="5">5 - Excellent</option>
+                    </Form.Control>
+                  )}
+                />
+              </Form.Group>
+              <Form.Group controlId="reviewText">
+                <Form.Label>Review</Form.Label>
+                <Controller
+                  name="reviewText"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Form.Control as="textarea" rows={3} {...field} required />
+                  )}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit Review
+              </Button>
+            </Form>
+          )}
         </Col>
       </Row>
     </Container>
